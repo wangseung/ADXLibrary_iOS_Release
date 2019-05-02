7cc3a2f950fcb9338a707243f7ec348542bf7210
Chiung Choi <god@adxcorp.kr>
2019-05-02 16:11:38 +0900

    [Modify] update package script

--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
 pod repo push ADXLibrary ADXLibrary.podspec --allow-warnings --verbose
+pod repo push ADXLibrary ADXLibrary-AdFit.podspec --allow-warnings --verbose
+pod repo push ADXLibrary ADXLibrary-UnityAds.podspec --allow-warnings --verbose
+pod repo push ADXLibrary ADXLibrary-Vungle.podspec --allow-warnings --verbose
