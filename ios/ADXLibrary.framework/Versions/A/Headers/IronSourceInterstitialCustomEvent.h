c71ce167b6b20219b6626de1b53a3cf316dc2738
Chiung Choi <god@adxcorp.kr>
2019-09-26 13:36:18 +0900

    [Modify] update 1.6.0

--- ios/ADXLibrary.framework/Versions/A/Headers/IronSourceInterstitialCustomEvent.h
+++ ios/ADXLibrary.framework/Versions/A/Headers/IronSourceInterstitialCustomEvent.h
@@ -3,15 +3,18 @@
 //
 
 #if __has_include(<MoPub/MoPub.h>)
-#import <MoPub/MoPub.h>
+    #import <MoPub/MoPub.h>
 #elif __has_include(<MoPubSDKFramework/MoPub.h>)
-#import <MoPubSDKFramework/MoPub.h>
+    #import <MoPubSDKFramework/MoPub.h>
 #else
-#import "MPInterstitialCustomEvent.h"
+    #import "MPInterstitialCustomEvent.h"
 #endif
 #import <IronSource/IronSource.h>
+#import "IronSourceInterstitialDelegate.h"
+#import "IronSourceManager.h"
+#import "IronSourceUtils.h"
 
-@interface IronSourceInterstitialCustomEvent : MPInterstitialCustomEvent <ISDemandOnlyInterstitialDelegate>
+@interface IronSourceInterstitialCustomEvent : MPInterstitialCustomEvent
 
 
 @end
