de98b19da6ab8b75170c8b607b8f7db5961b9a46
YongSun Kim <sunny@gomfactory.com>
2020-05-14 16:14:57 +0900

    [Modify] remove AdFit SDK in shell script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 pod repo push ADXLibrary ADXLibrary.podspec --allow-warnings --verbose
-pod repo push ADXLibrary ADXLibrary-AdFit.podspec --allow-warnings --verbose
+#pod repo push ADXLibrary ADXLibrary-AdFit.podspec --allow-warnings --verbose
 pod repo push ADXLibrary ADXLibrary-UnityAds.podspec --allow-warnings --verbose
 pod repo push ADXLibrary ADXLibrary-Vungle.podspec --allow-warnings --verbose
 pod repo push ADXLibrary ADXLibrary-AdColony.podspec --allow-warnings --verbose
