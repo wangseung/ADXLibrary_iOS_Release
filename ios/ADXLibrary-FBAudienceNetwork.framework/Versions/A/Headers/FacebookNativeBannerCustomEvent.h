8809b4b25c519d77adf634cdf9479f190445d831
YongSun Kim <sunny@gomfactory.com>
2020-07-08 16:31:13 +0900

    [Modify] update framework (1.7.3)

---
+++ ios/ADXLibrary-FBAudienceNetwork.framework/Versions/A/Headers/FacebookNativeBannerCustomEvent.h
@@ -0,0 +1,19 @@
+//
+//  FacebookNativeBannerCustomEvent.h
+//  MoPub
+//
+//  Copyright (c) 2014 MoPub. All rights reserved.
+//
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+    #import <MoPubSDKFramework/MoPub.h>
+#else
+    #import "MPNativeCustomEvent.h"
+#endif
+
+
+@interface FacebookNativeBannerCustomEvent : MPNativeCustomEvent
+
+@end
