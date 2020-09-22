f03714d2d4573f00c72fab858cd7e3f9ae0fd5e6
Chiung Choi <god@adxcorp.kr>
2020-09-22 20:48:38 +0900

    [Modify] XCode framework 테스트

---
+++ ios/ADXLibrary.framework/Headers/ADXLibrary.h
@@ -0,0 +1,17 @@
+//
+//  ADXLibrary.h
+//  ADXLibrary
+//
+//  Created by 최치웅 on 2020/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADXLibrary.
+FOUNDATION_EXPORT double ADXLibraryVersionNumber;
+
+//! Project version string for ADXLibrary.
+FOUNDATION_EXPORT const unsigned char ADXLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADXLibrary/PublicHeader.h>
+
--- ios/ADXLibrary.framework/Headers/NativeAdFactory.h
+++ ios/ADXLibrary.framework/Headers/NativeAdFactory.h
@@ -6,6 +6,9 @@
 //
 //
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 @class MPNativeAd;
 @class MPTableViewAdPlacer;
 @class MPCollectionViewAdPlacer;
