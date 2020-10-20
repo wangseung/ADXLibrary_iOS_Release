6d0a3f0e064ea3999ce6b6f88ebb7ce1e5210bbf
YongSun Kim <sunny@gomfactory.com>
2020-10-20 18:01:07 +0900

    [Modify] add --skip-import-validation

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-pod repo push ADXLibrary ADXLibrary.podspec --allow-warnings --verbose
+pod repo push ADXLibrary ADXLibrary.podspec --allow-warnings --verbose --skip-import-validation
 #pod repo push ADXLibrary ADXLibrary-AdFit.podspec --allow-warnings --verbose
-pod repo push ADXLibrary ADXLibrary-UnityAds.podspec --allow-warnings --verbose
-pod repo push ADXLibrary ADXLibrary-Vungle.podspec --allow-warnings --verbose
-pod repo push ADXLibrary ADXLibrary-AdColony.podspec --allow-warnings --verbose
-pod repo push ADXLibrary ADXLibrary-FBAudienceNetwork.podspec --allow-warnings --verbose
+pod repo push ADXLibrary ADXLibrary-UnityAds.podspec --allow-warnings --verbose --skip-import-validation
+pod repo push ADXLibrary ADXLibrary-Vungle.podspec --allow-warnings --verbose --skip-import-validation
+pod repo push ADXLibrary ADXLibrary-AdColony.podspec --allow-warnings --verbose --skip-import-validation
+pod repo push ADXLibrary ADXLibrary-FBAudienceNetwork.podspec --allow-warnings --verbose --skip-import-validation
