76f3cae072833eab96b8da4994d5aa5172ea0211
YongSun Kim <sunny@gomfactory.com>
2020-12-11 11:17:01 +0900

    [Modify] version up

--- MediationAdapter/ADXLibrary/Classes/GDPR/ADXGDPR.h
+++ MediationAdapter/ADXLibrary/Classes/GDPR/ADXGDPR.h
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import "ADXConsentViewController.h"
 
-#define ADX_SDK_VERSION @"1.8.2"
+#define ADX_SDK_VERSION @"1.8.4"
 
 typedef NS_ENUM(NSInteger, ADXConsentState) {
     ADXConsentStateUnknown      = 0,
