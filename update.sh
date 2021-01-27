582dee43078e5b34b8bdf040585597b8cddd6829
YongSun Kim <sunny@gomfactory.com>
2021-01-27 16:47:49 +0900

    [Modify] remove AdColony [Modify] add Cauly and Pangle

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@ pod repo push ADXLibrary ADXLibrary.podspec --allow-warnings --verbose --skip-im
 #pod repo push ADXLibrary ADXLibrary-AdFit.podspec --allow-warnings --verbose
 pod repo push ADXLibrary ADXLibrary-UnityAds.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-Vungle.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
-pod repo push ADXLibrary ADXLibrary-AdColony.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
+#pod repo push ADXLibrary ADXLibrary-AdColony.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
 pod repo push ADXLibrary ADXLibrary-FBAudienceNetwork.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
+pod repo push ADXLibrary ADXLibrary-Cauly.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
+pod repo push ADXLibrary ADXLibrary-Pangle.podspec --allow-warnings --verbose --skip-import-validation --use-libraries
